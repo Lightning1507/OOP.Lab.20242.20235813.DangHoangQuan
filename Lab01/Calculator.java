1be049ab5182df956b861c650d9e9db96dca52c1
Đặng Hoàng Quân <lightning1575@gmail.com>
2025-03-01 14:44:20 +0700

    Update Calculator.java

--- Lab01/Calculator.java
+++ Lab01/Calculator.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 
-public class Calculate2{
+public class Calculator{
     public static void main(String[] args){
         String s1,s2;
         s1 = JOptionPane.showInputDialog("Enter the first number: ");
