8fe8ea64492ffa0f16765bec2a90121b672bed91
Lightning <lightning1575@gmail.com>
2025-03-01 14:03:08 +0700

    committing lab01

--- Lab01/Calculate2.java
+++ Lab01/Calculate2.java
@@ -7,6 +7,14 @@ public class Calculate2{
         s2 = JOptionPane.showInputDialog("Enter the second number: ");
         double num1 = Double.parseDouble(s1);
         double num2 = Double.parseDouble(s2);
+        if (num2==0){
+            JOptionPane.showMessageDialog(null, "Result of sum "+(num1+num2) + "\n" + 
+        "Result of subtraction "+(num1-num2) + 
+        "\n" + "Result of division: Cannot" + 
+        "\n" + "Result of multiplication "+(num1*num2) + 
+        "\n" + "Result of remainder: Cannot ");
+            System.exit(0);
+        }
         JOptionPane.showMessageDialog(null, "Result of sum "+(num1+num2) + "\n" + 
         "Result of subtraction "+(num1-num2) + 
         "\n" + "Result of division "+(num1/num2) + 
